d888e03f448dfbd036cdceb0a12bf286d5547c18
Kaspar Tvedt Moberg <kaspar.moberg@student.uib.no>
2025-12-02 14:35:13 +0100

    feat: fast(er) 02 for good measure :)

--- 02/main.go
+++ 02/main.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	task01()
 	task02()
+	task02Better()
 }
 
 func task01() {
@@ -78,6 +79,44 @@ func task02() {
 			}
 			if match != nil && len(match.String()) == len(stringId) {
 				sumInvalidIds += id				
+			}
+		}
+	}
+
+	fmt.Println(sumInvalidIds)
+}
+
+func task02Better() {
+	inp, err := os.ReadFile("input")
+	if err != nil {
+		panic(err)
+	}
+
+	idRanges := strings.Split(strings.TrimSpace(string(inp)), ",")
+	var sumInvalidIds int64 = 0
+
+
+	for _, idRange := range idRanges {
+		splitRange := strings.Split(idRange, "-")
+		lowerInt, err := strconv.ParseInt(splitRange[0], 10, 64)
+		if err != nil {
+			panic(err)	
+		}
+
+		upperInt, err := strconv.ParseInt(splitRange[1], 10, 64)
+		if err != nil {
+			panic(err)	
+		}
+
+		for id := lowerInt; id <= upperInt; id++ {
+			stringId := strconv.FormatInt(id, 10)
+			idLen := len(stringId)
+			for i := 1; i < len(stringId) / 2 + 1; i++ {
+				subStr := stringId[:i]
+				if idLen % i == 0 && strings.Repeat(subStr, idLen / i) == stringId {
+					sumInvalidIds += id	
+					break
+				}
 			}
 		}
 	}
